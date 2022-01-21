b445621374724fcc28b19279f81d39d31c004a4a
benhtan <btan90@gmail.com>
2022-01-21 13:38:24 -0800

    find_AllMotion working

--- AllmotionPythonExample.py
+++ AllmotionPythonExample.py
@@ -7,64 +7,49 @@
 import serial
 import time
 
-#ser.close()  
-
-def serial_ports():
-    ports = ['COM%s' % (i + 1) for i in range(256)]
-
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
-
-# See which com port shows up and change "COM5" in code below to that com port
-if __name__ == '__main__':
-    print(serial_ports()) 
-   
-    ser = serial.Serial(
-    port='COM5',
-    baudrate=9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS
-)
+# read response
+def read_response(ser):
+    return ser.read_until(expected=b'\r\n')
 
 # ser has to be serial.Serial object. command is b'/1 command string'
-def send_command_then_read_resp(ser, command):
+def send_command_then_wait_for_ready(ser, command):
     ser.write(command)
-    return ser.read_until(expected=b'\r\n',size=8)
+    waitForReady(ser)
 
 # ser has to be serial.Serial object
 def waitForReady(ser):
     while True:
         ser.write(b'/1Q\r\n')
-        if b'/0`' in ser.read_until(expected=b'\r\n',size=8):
+        resp = read_response(ser)
+        # print(resp)
+        if b'/0`' in resp:
             break
 
 # find AllMotion in list of COM ports
 def find_AllMotion():
-    ports = ['COM%s' % (i + 1) for i in range(256)]
+    ports = ['COM%s' % (i + 1) for i in range(0,256)]
 
-    result = []
     for port in ports:
         try:
-            s = serial.Serial(port=port, baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
-            s.write(b'/1&\r\n') # query board version
-            s.close()
-            result.append(port)
+            print(f'Checking {port}')
+            ser = serial.Serial(port=port, baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, write_timeout=0.5, timeout=0.5)
+            ser.write(b'/1&\r\n') # query board version
+            resp = read_response(ser)
+            if b'EZController AllMotion' in resp:
+                print(f'Found AllMotion: {port}')
+                return ser
+            else:
+                ser.close()
         except (OSError, serial.SerialException):
             pass
-    return result
+    return None
 
-# ser.write(b'/1aM1aE1000V5000L10Z116800R\r\n')   # Home
-# waitForReady(ser)
-# ser.write(b'/1aM1V59900L30A100000R\r\n')   # Go forward
-# waitForReady(ser)
-# ser.write(b'/1aM1aE1000V5000L10Z116800R\r\n')   # Home
-# waitForReady(ser)
+
+# send_command_then_wait_for_ready(ser, b'/1aM1aE1000V5000L10Z116800R\r\n')
+# send_command_then_wait_for_ready(ser, b'/1aM1V59900L30A100000R\r\n')
+# send_command_then_wait_for_ready(ser, b'/1aM1aE1000V5000L10Z116800R\r\n')
+ser = find_AllMotion()
+send_command_then_wait_for_ready(ser, b'/1aM1aE1000V5000L10Z116800R\r\n')
+send_command_then_wait_for_ready(ser, b'/1aM1aE1000V5000L10Z116800R\r\n')
+send_command_then_wait_for_ready(ser, b'/1aM1V59900L30A100000R\r\n')
 ser.close()  
