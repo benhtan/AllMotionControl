840e726005643108ca223b6030709eeb34decfe3
benhtan <btan90@gmail.com>
2022-01-21 12:51:12 -0800

    waitForReady working

--- AllmotionPythonExample.py
+++ AllmotionPythonExample.py
@@ -24,8 +24,7 @@ def serial_ports():
 
 # See which com port shows up and change "COM5" in code below to that com port
 if __name__ == '__main__':
-    print(serial_ports())
- 
+    print(serial_ports()) 
    
     ser = serial.Serial(
     port='COM5',
@@ -35,8 +34,37 @@ if __name__ == '__main__':
     bytesize=serial.EIGHTBITS
 )
 
-# ser.write(b'/1aM1V781L30Z18045R\r\n')
-ser.write(b'/1Q\r\n')
-time.sleep(0.5)
-print(ser.read(size=5).decode("utf-8"))
+# ser has to be serial.Serial object. command is b'/1 command string'
+def send_command_then_read_resp(ser, command):
+    ser.write(command)
+    return ser.read_until(expected=b'\r\n',size=8)
+
+# ser has to be serial.Serial object
+def waitForReady(ser):
+    while True:
+        ser.write(b'/1Q\r\n')
+        if b'/0`' in ser.read_until(expected=b'\r\n',size=8):
+            break
+
+# find AllMotion in list of COM ports
+def find_AllMotion():
+    ports = ['COM%s' % (i + 1) for i in range(256)]
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port=port, baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+            s.write(b'/1&\r\n') # query board version
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+# ser.write(b'/1aM1aE1000V5000L10Z116800R\r\n')   # Home
+# waitForReady(ser)
+# ser.write(b'/1aM1V59900L30A100000R\r\n')   # Go forward
+# waitForReady(ser)
+# ser.write(b'/1aM1aE1000V5000L10Z116800R\r\n')   # Home
+# waitForReady(ser)
 ser.close()  
